5abf585aaa9733bf823ba81d548a56078e248b0e
EigenFace <fox.petrarch@gmail.com>
2019-02-14 09:45:03 -0500

    Real Initial Commit

--- getAuthors.js
+++ getAuthors.js
@@ -1,6 +1,9 @@
 var g = document.getElementsByClassName('commentarea');
 var authors = {};
 for(let item of g[0].getElementsByClassName('author')) {
+    if(authors[item.text] == undefined) {
+        authors[item.text] = new Object();
+    } 
     let currCount = authors[item.text].count;
     if(currCount == undefined) {
         currCount = 0;
@@ -13,13 +16,17 @@ var tuples = [];
 
 for (var key in authors) tuples.push([key, authors[key].count]);
 
-tuples.sort(function(a,b) { return a[1] - b[1]});
+tuples.sort(function(a,b) { return b[1] - a[1]});
 
 topFive = tuples.slice(0, 5);
 console.log(topFive);
 
+function exists(arr, search) {
+    return arr.some(row => row.includes(search));
+}
+
 for(let item of g[0].getElementsByClassName('author')) {
-    if(topFive.includes(item.text))
+    if(exists(topFive,item.text))
     {
         item.style.backgroundColor = '#e8453c';
     }
